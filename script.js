20afea9d90c53f7bf5ff75ddb2a691f84f1aa127
John McCutchen <purplegrits@yahoo.com>
2019-11-16 13:34:46 -0500

    added links in JS file

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#weather").on("click", () =>{
+    window.open("https://jmccutchen.github.io/WeatherApp/", '_blank')
+});
+
+$("#deet").on("click", () =>{
+    window.open("https://thebrellis.github.io/Project-1/", '_blank')
+});
+
+$("#planner").on("click", () =>{
+    window.open("https://jmccutchen.github.io/DayPlanner/", '_blank')
+});
+
+$("#code").on("click", () =>{
+    window.open("https://jmccutchen.github.io/Code-quiz/", '_blank')
+});
